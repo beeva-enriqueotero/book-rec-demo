f5f385ad24b8a27a1fa26783d747c1164f560f30
Marian C Moldovan <marian.claudiu@beeva.com>
2014-11-27 17:57:40 +0100

    Cover and Search endpoints added

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require( 'express' );
+var http = require( 'http' );
+var path  = require( 'path' );
+var scraperjs = require('scraperjs');
+
+var app    = express();
+
+var db = require("mongojs").connect('mongodb://localhost/books', ['books']);
+
+// all environments
+app.set( 'port', process.env.PORT || 8080 );
+app.get(  '/cover', function ( req, res, next ){
+  var query = encodeURIComponent(req.query['query']).replace('%20','+');
+  var url = 'http://bigbooksearch.com/query.php?SearchIndex=books&Keywords=' + query + '&ItemPage=1';
+  scraperjs.StaticScraper.create(url)
+    .scrape(function($) {
+        return $("img").map(function() {
+            return {src:$(this).attr('src'), title:$(this).attr('alt')};
+        }).get();
+    }, function(books) {
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify(books));
+    })
+});
+
+app.get(  '/search', function ( req, res, next ){
+  var query = encodeURIComponent(req.query['query']).replace('%20','+');
+  db.books.find({t: {$regex:query.toUpperCase()}}, function(err, docs) {
+    if( err || !docs) {
+      console.log("No female users found");
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify([]));
+    }
+    else{
+      var arr = []
+      docs.forEach(function(item) { arr.push({"id": item.i, "t" : item.t }); });
+      var result = {books:arr.slice(0, 50)};
+      res.setHeader('Content-Type', 'application/json');
+      return res.end(JSON.stringify(result));
+    }
+  });
+});
+
+
+
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
